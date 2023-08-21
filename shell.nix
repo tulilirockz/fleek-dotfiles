90b3c8fb1eec5fb18f8c84d3d97971ff9a665f90
tulili <tulilirockz.pub+git@gmail.com>
2023-08-21 02:49:59 -0300

    join new system

--- shell.nix
+++ shell.nix
@@ -12,7 +12,12 @@
 };
     # bash
   programs.bash.profileExtra = "[ -r ~/.nix-profile/etc/profile.d/nix.sh ] && source  ~/.nix-profile/etc/profile.d/nix.sh";
-  programs.bash.initExtra = "source <(fleek completion bash)";
+  programs.bash.initExtra = ''
+    if [ -f /etc/bashrc ]; then
+        . /etc/bashrc
+    fi
+    source <(fleek completion bash)
+  '';
   programs.bash.enableCompletion = true;
   programs.bash.enableVteIntegration = true;
   programs.bash.enable = true;
